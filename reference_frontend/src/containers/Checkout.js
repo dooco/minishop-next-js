afd04ffb5b8f749d9175f62dcb62d776ee80c9c1
Padraig Dooley <padraigdooley@gmail.com>
2024-06-05 21:22:23 +0100

    initial commit

---
+++ reference_frontend/src/containers/Checkout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const checkout = () => (
+    <div>
+        <h1>Checkout page</h1>
+    </div>
+);
+
+export default checkout;
---
+++ reference_frontend/src/containers/ThankYou.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const thankYou = () => (
+    <div>
+        <h1>Thank you page</h1>
+    </div>
+);
+
+export default  thankYou;
