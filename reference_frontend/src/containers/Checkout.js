d5b9d971da3da871dc2e3b33ff080ad1de3c219c
Padraig Dooley <padraigdooley@gmail.com>
2024-06-06 12:10:28 +0100

    remove reference frontend

--- reference_frontend/src/containers/Checkout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const checkout = () => (
-    <div>
-        <h1>Checkout page</h1>
-    </div>
-);
-
-export default checkout;
--- reference_frontend/src/containers/ThankYou.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const thankYou = () => (
-    <div>
-        <h1>Thank you page</h1>
-    </div>
-);
-
-export default  thankYou;
